007ed2c3846f24e2a36329f4045258bffef532fd
Ashok Vyas <ashokvyas82@gmail.com>
2019-02-13 11:20:05 +0530

    html content

---
+++ Creating HTML Content/dom.js
@@ -0,0 +1,18 @@
+let main = document.querySelector(".main");
+let h2 = main.querySelector("h2");
+h2.textContent = "Loading Movies";
+
+let p = main.querySelector('p');
+p.innerHTML = 'And Now a list of <strong>Movies</strong>.';
+
+let ul = document.createElement('ul');
+main.appendChild(ul);
+
+movies.forEach(function(item) {
+    let li = document.createElement('li');
+    let txt = document.createTextNode(item);
+    li.appendChild(txt);
+    ul.appendChild(li);    
+});
+
+main.removeChild(ul);
---
+++ Creating HTML Content/movies.js
@@ -0,0 +1,9 @@
+let movies =['All of Me',
+    'All the President Men',
+    'All the Right Moves',
+    'Almost Famous',
+    'Almost Heroes',
+    'Along Came a Spider',
+    'Alpha Dog',
+    'Alpha and Omega'
+    ];
